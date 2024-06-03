4f2a85c540e4b530b5e0489666ebca58e6bdb4fd
KxlePH <155553798+kxle0801@users.noreply.github.com>
2024-06-03 21:46:42 +0800

    Add files via upload

---
+++ src/kxle/command/subcommands/LootboxList.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * Github: https://github.com/kxle0801
+ * Author: KxlePH
+ */
+
+declare(strict_types = 1);
+
+namespace kxle\command\subcommands;
+
+use kxle\Main;
+
+use kxle\utils\SoundUtils;
+use kxle\utils\SourceUtils;
+use kxle\utils\PermissionIds;
+
+use pocketmine\command\CommandSender;
+
+use CortexPE\Commando\BaseSubCommand;
+
+class LootboxList extends BaseSubCommand {
+
+	/**
+	 * @return void
+	 */
+	public function prepare(): void {
+		$this->setPermission(PermissionIds::KXLOOTBOX_LIST);
+	}
+
+	/**
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+		$plugin = Main::getInstance();
+		$config = $plugin->getConfig();
+		$message = SourceUtils::getMessages();
+		$sound = SourceUtils::getSounds();
+		
+		$lootboxData = SourceUtils::getLootboxData()->getAll();
+		$looboxInfo = [];
+		foreach ($lootboxData as $identifier => $data) {
+			$name = $data['name'];
+			$looboxInfo[$identifier] = $name;
+		}
+
+		if (empty($looboxInfo)) {
+			$sender->sendMessage($config->get("prefix") . " " . str_replace("{base-cmd}", $config->get("base-cmd"), $message->get("sub-cmd-NoBoxes")));
+			SoundUtils::send($sender, $sound->get("sound-InvalidAction"));
+			return;
+		}
+
+		$count = 1;
+		$sender->sendMessage("§r§7Lists of created lootboxes:\n");
+		foreach ($looboxInfo as $identifier => $name) {
+			$sender->sendMessage(str_replace(["{count}", "{name}", "{identifier}"], [$count, $name, $identifier], $config->get("sub-cmd-list-format")));
+			$count++;
+		}
+		SoundUtils::send($sender, $sound->get("sound-cmd-List"));
+	}
+}
---
+++ src/kxle/event/EventListener.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ * Github: https://github.com/kxle0801
+ * Author: KxlePH
+ */
+
+declare(strict_types = 1);
+
+namespace kxle\event;
+
+use kxle\Main;
+
+use kxle\utils\ItemUtils;
+use kxle\utils\SoundUtils;
+use kxle\utils\SourceUtils;
+use kxle\inventory\LootboxMenu;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+
+class EventListener implements Listener {
+
+    /**
+     * @param PlayerInteractEvent $event
+     */
+    public function onInteract(PlayerInteractEvent $event): void {
+        $action = $event->getAction();
+        $player = $event->getPlayer();
+        $item = $event->getItem();
+        $tag = $item->getNamedTag();
+        
+        if ($tag->getTag(ItemUtils::TAG_LOOTBOX) && $tag->getTag(ItemUtils::TAG_LOOTBOX_IDENTIFIER)) {
+            $plugin = Main::getInstance();
+		    $config = $plugin->getConfig();
+            $message = SourceUtils::getMessages();
+            $sound = SourceUtils::getSounds();
+            
+            $lootboxIdentifier = $tag->getTag(ItemUtils::TAG_LOOTBOX_IDENTIFIER)->getValue();
+            $lootboxData = SourceUtils::getLootboxData()->get($lootboxIdentifier);
+
+            if (!is_array($lootboxData)) return;
+
+            $inventory = $player->getInventory();
+            $inventorySize = $inventory->getSize();
+            $inventoryItemsCount = count($inventory->getContents());
+            $contents = ItemUtils::decodeContent($lootboxData['contents']);
+            
+            $event->cancel();
+            switch ($action) {
+                case PlayerInteractEvent::LEFT_CLICK_BLOCK:
+                    $inventorySpace = $config->get("lootbox-max-rewards");
+                    if (($inventoryItemsCount + $inventorySpace) > $inventorySize) {
+                        $player->sendMessage($config->get("prefix") . " " . $message->get("sub-cmd-NoSpace"));
+                        SoundUtils::send($player, $sound->get("sound-InvalidAction"));
+                        $event->cancel();
+                        return;
+                    }
+
+                    shuffle($contents);
+
+                    $maxRewards = 0;
+                    foreach ($contents as $content) {
+                        if (is_string($content)) {
+                            $lootboxItem = ItemUtils::decodeItem($content);
+                            $inventory->addItem($lootboxItem);
+                            $maxRewards++;
+                            
+                            if ($maxRewards >= $config->get("lootbox-max-rewards")) break;
+                        }
+                    }
+                    $player->sendMessage($config->get("prefix") . " " . str_replace("{name}", $lootboxData['name'], $message->get("lb-claim-Opened")));
+                    $inventory->setItemInHand($item->setCount($item->getCount() - 1));
+                    SoundUtils::send($player, $sound->get("sound-Claimed"));
+                    break;
+                case PlayerInteractEvent::RIGHT_CLICK_BLOCK:
+                    if (!$config->get("preview")) return;
+                    
+                    LootboxMenu::send($player, $lootboxData);
+                    SoundUtils::send($player, $sound->get("sound-Preview"));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }    
+}
